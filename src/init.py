d485816bdf89291c1b9b3773a642c482e3b74f69
Tizian Imseng <tizian.imseng@bluewin.ch>
2024-08-02 08:54:20 +0200

    update
    + Main Application can only be accessed if surname and password is not NULL
    
    + on start of main, login window is killed
    
    +Added CSV

--- src/init.py
+++ src/init.py
@@ -1,51 +1,50 @@
 import tkinter as tk
 import customtkinter as ctk
+import csv
 
 # Theme of Application
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("yellow")
+ctk.set_default_color_theme("blue")
 ctk.deactivate_automatic_dpi_awareness()
 
 app = ctk.CTk()
 
 def login_screen():
-    root = ctk.CTk()
-    
     # Create Windows for GUI
-    root.title("pt1 - Lightweight Notes")
-    root.geometry('350x350')
-    root.resizable(False, False)
+    app.title("pt1 - Lightweight Notes")
+    app.geometry('350x350')
+    app.resizable(False, False)
     
-    title = ctk.CTkLabel(root, text="Login", font=('Bold Calibri', 25))
-    title.place(relx=0.3, rely=0.1)
+    title = ctk.CTkLabel(app, text="Login", font=('Bold Calibri', 25))
+    title.place(relx=0.40625, rely=0.1)
     
-    surname = ctk.CTkEntry(root, placeholder_text="Surname")
+    surname = ctk.CTkEntry(app, placeholder_text="Surname")
     surname.place(relx=0.1, rely=0.3, relwidth=0.8)
     
-    name = ctk.CTkEntry(root, placeholder_text="Name")
-    surname.place(relx=0.1, rely=0.5, relwidth=0.8)
+    password = ctk.CTkEntry(app, placeholder_text="Password")
+    password.configure(show="*")
+    password.place(relx=0.1, rely=0.5, relwidth=0.8)
     
-    login_button = ctk.CTkButton(root, text="Login", command=lambda: login(surname))
+    login_button = ctk.CTkButton(app, text="Login", command=lambda: login(surname, password))
     login_button.place(relx=0.1, rely=0.7, relwidth=0.8)
     
-    root.mainloop()
 
-def login(surname, root):
-    if not len(surname.get())  == 0:
-        root.quit()
+def login(surname, password):
+    if not (len(surname.get()) and len(password.get())) == 0:
         application()
         
-        
+
 def application():
-    app = ctk.CTk()
+    for widget in app.winfo_children():
+        widget.destroy()
+    
     app.title("pt1 - Lightweight Notes - Application")
     app.geometry('500x500')
-    app.resizable(False, False)
     
     label = ctk.CTkLabel(app, text="Welcome to the Application", font=('Bold Calibri', 25))
     label.place(relx=0.1, rely=0.1)
     
-    app.mainloop()
 
 # Start the login screen
 login_screen()
+app.mainloop()
