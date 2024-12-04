6727052f8ac198e3067b92cc1a07a1e83e86756c
Tizian Imseng <tizian.imseng@bluewin.ch>
2024-12-04 16:20:43 +0100

    [Update] init.py; create_account.py
    + initialize function to create a new account
    
    + Fix bug where toplevel window is initialised when init.py is started.

--- src/create_account.py
+++ src/create_account.py
@@ -1,12 +1,24 @@
 import tkinter as tk
 import customtkinter as ctk
+from PIL import Image, ImageTk
+
+from init import runpath
+
 
 def create_account(self):
     if self.create_account_window is None or not self.create_account_window.winfo_exists():
             self.create_account_window = ctk.CTkToplevel(self.master)
             self.create_account_window.title("pt1 Lightweight Notes - Create Account")
-            self.create_account_window.geometry("500x500")
+            self.create_account_window.geometry("900x600")
             self.create_account_window.resizable(False, False)
+
+            background_image = ctk.CTkImage(Image.open(f"{runpath}\\src\\img\\signup_background.jpg"), size=(900, 600))
+
+            background_label = ctk.CTkLabel(self.create_account_window, image=background_image, text="")
+            background_label.place(relwidth=1, relheight=1)
+
+            # Next do section for holding different entries with plain background color
+
             self.create_account_window.after(100, self.create_account_window.lift)
 
             
