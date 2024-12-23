8b86a3593fe91c547a62da0c32ff13ca629e882c
Tizian Imseng <tizian.imseng@bluewin.ch>
2024-12-23 15:29:01 +0100

    [Update] init.py
    + remove whitespace for python interpreter to recognize that init_creation is called as a lambda function

--- src/tempCodeRunnerFile.py
+++ src/tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-def display_message(self, message, color, duration=None):
-        """
-        Displays a message on the login screen.
-        """
-        if self.message_label:
-            self.message_label.destroy()
-
-        self.message_label = ctk.CTkLabel(self.master, text=message, font=('Bold Calibri', 12), text_color=color)
-        self.message_label.place(relx=0.1, rely=0.55, relwidth=0.8)
-
-        if duration:
-            self.master.after(duration, self.message_label.destroy)
+runpath
