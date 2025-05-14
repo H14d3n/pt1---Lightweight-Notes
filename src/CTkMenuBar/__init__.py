33596c0e8dd96ad963778d4b743d7cdf36b1cf32
limafresh <173814432+limafresh@users.noreply.github.com>
2025-05-14 12:49:26 +0300

    Fixed TclError bug

---
+++ src/CTkMenuBar/__init__.py
@@ -0,0 +1,13 @@
+"""
+MenuBar widget for customtkinter
+by Akascape
+License: MIT
+Homepage: https://github.com/Akascape/CTkMenuBar
+"""
+# some modifed version
+
+__version__ = '0.8'
+
+from .menu_bar import CTkMenuBar
+from .title_menu_win import CTkTitleMenu
+from .dropdown_menu import CustomDropdownMenu
--- src/init.py
+++ src/init.py
@@ -18,8 +18,6 @@ from create_account import *
 ToDo:
  * Implement"Change Font" functionality
  * Implement "Export" functionalities
- * Implement "Theme change" functionality
- ? Fix bad window path name bug (tkinter.TclError)
 """
 
 # Global configurations
